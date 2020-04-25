aed821b17ffe0659fe3f5acc0480e5f9dd20e442
Marcin <mieker@wp.pl>
2020-04-25 10:30:07 +0200

    early log panel implementation ready

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
   <div>
-    <h1>Twoj adres e-mail to {{email}}</h1>
-    <div v-if="email.length < 10 ">Ale masz krotki adres!</div>
-    <div v-else-if="email.length < 15 ">Twoj adres jest w sam raz</div>
-    <div v-else>Twoj adres jest za dlugi!!</div>
-    <input type="email" v-model="email">
-    <button @click="alertMyEmail">Wyswietl moj e-mail</button>
-  </div>
+    <h1>Witaj w systemie do zapisow na zajecia</h1>
+    <div v-if="logButtonPressed == false">
+      <p>Zaloguj sie e-mailem</p>
+      <input type="email" v-model="email">
+      <button @click="logButtonPressed = true">Wchodze</button>
+    </div>
+    <div v-else>
+      <h2>Witaj {{email}}</h2>
+      <a href="">Wyloguj</a>
+    </div>
+   </div>
 </template>
 
 <script>
 export default {
   data () {
     return {
-      email: ''
+      email: '',
+      logButtonPressed: false
     };
   },
 
   methods: {
     alertMyEmail() {
       alert(this.email);
-    }
+    },
+    
   }
 }
 </script>
