78b0a1aa7dc44834075a433fd5895f6f195b0ecc
Marcin <mieker@wp.pl>
2020-04-25 10:05:39 +0200

    length of email status implemented

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h1>Twoj adres e-mail to {{email}}</h1>
+    <div v-if="email.length < 10 ">Ale masz krotki adres!</div>
+    <div v-else-if="email.length < 15 ">Twoj adres jest w sam raz</div>
+    <div v-else>Twoj adres jest za dlugi!!</div>
+
     <input type="email" v-model="email">
   </div>
 </template>
